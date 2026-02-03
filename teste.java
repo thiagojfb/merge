d9efdc0c9df6eeef373523e1ca7f783a821a60e3
THIAGO-PC\thiag <thiagojfb@gmail.com>
2026-02-03 16:51:50 -0300

    criado metodo de subtração e mudança do nome para joão

--- teste.java
+++ teste.java
@@ -3,10 +3,15 @@ class teste{
         System.out.println("Hello, World!");
 
         teste t = new teste();
-        System.out.println(t.greet("Alice"));
+        System.out.println(t.greet("João"));
     }
 
     public String greet(String name) {
         return "Hello, " + name + "!";
+    }
+
+    //subtração
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
